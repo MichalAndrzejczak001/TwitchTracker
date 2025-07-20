7f82063a3cf79fbb3067ac96f7e1e61559ef21ef
MichalAndrzejczak001 <michandrzejczakofficial@gmail.com>
2025-07-20 16:12:28 +0200

    Backend starting project.

---
+++ TwitchTracker/src/test/java/spring/project/twitchtracker/TwitchTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package spring.project.twitchtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitchTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
